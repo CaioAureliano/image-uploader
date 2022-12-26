f06ed296d222f708fe731a8b81ec36314ee791cb
Caio Aureliano <caio.aureliano1@gmail.com>
2022-12-25 22:31:05 -0300

    refactor(*): functions to start web server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,23 +1,3 @@
-import express, { Express } from "express";
-import pino from "pino-http";
-import fileUpload from "express-fileupload";
-import { sessionMiddleware } from "./app/middleware/session";
-import { uncaughtErrorListener } from "./app/middleware/uncaught-error-listener";
-import { router } from "./app/router/router";
-import swaggerUI from "swagger-ui-express";
-import { swaggerSpecs } from "../docs/swagger";
-import { acceptHeaderMiddleware } from "./app/middleware/accept-header";
-    
-const app: Express = express();
+import { init } from "./server";
 
-sessionMiddleware(app);
-
-app.use(pino());
-app.use(fileUpload({ useTempFiles: true, tempFileDir: "tmp" }));
-app.use(acceptHeaderMiddleware);
-app.use(router());
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
-
-process.on("uncaughtException", uncaughtErrorListener);
-
-export default app;
+init();
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,5 +1,25 @@
-import app from "./index";
+import app from "./app/app";
 import config from "./app/config";
 import logger from "pino";
+import { Server } from "http";
 
-app.listen(config.port, () => logger().info("server started"));
+export const init = (): Promise<Server> => {
+    return new Promise((resolve, reject) => {
+        try {
+            const connection = app.listen(config.port, () => logger().info("server started"));
+            resolve(connection);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+export const stop = (connection: Server): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        try {
+            connection.close(() => resolve());
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
