c6b0d79b8500eedd5699bdb527b93f5145af4661
Caio Aureliano <caio.aureliano1@gmail.com>
2022-12-25 23:11:06 -0300

    refactor(modules): update path to modules

--- packages/server/src/modules/auth/client.ts
+++ packages/server/src/modules/auth/client.ts
@@ -1,5 +1,5 @@
 import { OAuth2Client } from "google-auth-library";
-import config from "../app/config";
+import config from "../../app/config";
 
 export default function GoogleOAuth2Client(): OAuth2Client {
     return new OAuth2Client(config.googleOAuth2ClientOptions);
