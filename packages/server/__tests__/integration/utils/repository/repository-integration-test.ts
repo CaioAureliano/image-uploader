b0883e79c570155b2fa6fe8361c5371b6446b1ba
Caio Aureliano <caio.aureliano1@gmail.com>
2023-01-02 23:23:04 -0300

    chore(upload test): repository helper to get and clean keys from redis

---
+++ packages/server/__tests__/integration/utils/repository/repository-integration-test.ts
@@ -0,0 +1,19 @@
+import cache from "../../../../src/app/database/redis";
+
+const cleanDatabase = async (): Promise<void> => {
+    const client = await cache().connection();
+    await client.sendCommand(["FLUSHALL"]);
+    await client.disconnect();
+};
+
+const getByKey = async (key: string): Promise<string> => {
+    const client = await cache().connection();
+    const result: string = await client.get(key) as string;
+    await client.disconnect();
+    return result;
+};
+
+export const repositoryCacheTest = {
+    cleanDatabase,
+    getByKey,
+};
