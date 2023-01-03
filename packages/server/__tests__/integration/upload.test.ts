07e62684ab4c88d8dbccaa6bcb595087c7502c5e
Caio Aureliano <caio.aureliano1@gmail.com>
2023-01-02 23:22:23 -0300

    test(upload): send file in /image endpoint and cached link on redis

--- packages/server/__tests__/integration/upload.test.ts
+++ packages/server/__tests__/integration/upload.test.ts
@@ -1,9 +1,11 @@
-import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "@jest/globals";
 import { Server } from "http";
 import path from "node:path";
 import request from "supertest";
+import { Response } from "superagent";
 import { destroy, setup } from "./utils/env/setup-environment";
-import { getFileGoogleApiRequestMock, setupMockRequests, tokenGoogleApiRequestMock, uploadGoogleApiRequestMock } from "./utils/mock/http/googleapi.mock";
+import { getFileGoogleApiRequestMock, GOOGLE_RESPONSE_ID_MOCK, GOOGLE_RESPONSE_THUMBNAILLINK_MOCK, setupMockRequests, tokenGoogleApiRequestMock, uploadGoogleApiRequestMock } from "./utils/mock/http/googleapi.mock";
+import { repositoryCacheTest } from "./utils/repository/repository-integration-test";
 
 describe("Upload [Integration]", () => {
     describe("/image", () => {
@@ -19,6 +21,10 @@ describe("Upload [Integration]", () => {
             await destroy(api);
         });
 
+        afterEach(async () => {
+            await repositoryCacheTest.cleanDatabase();
+        });
+
         describe("POST", () => {
 
             test("when upload valid file should be return 201 status and cache image link", async () => {
@@ -26,12 +32,15 @@ describe("Upload [Integration]", () => {
                 uploadGoogleApiRequestMock();
                 getFileGoogleApiRequestMock();
 
-                const response = await request(api)
+                const response: Response = await request(api)
                     .post("/image")
-                    .set("X-OAuth2-Code", "oauth2GoogleApiTest")
+                    .set("X-OAuth2-Code", "oauth2GoogleApiTestMock")
                     .attach("image", path.join(__dirname, "/utils/assets/jest.png"));
 
                 expect(response.status).toBe(201);
+                expect(response.body.id).toBeDefined();
+                expect(response.body.thumbnailLink).toBeDefined();
+                expect(await repositoryCacheTest.getByKey(GOOGLE_RESPONSE_ID_MOCK)).toBe(GOOGLE_RESPONSE_THUMBNAILLINK_MOCK);
             });
         });
     });    
--- packages/server/__tests__/integration/utils/mock/http/googleapi.mock.ts
+++ packages/server/__tests__/integration/utils/mock/http/googleapi.mock.ts
@@ -3,6 +3,10 @@ import nock from "nock";
 const OAUTH_GOOGLE_API_URL = "https://oauth2.googleapis.com";
 const GOOGLE_API_URL = "https://www.googleapis.com";
 
+export const GOOGLE_RESPONSE_ID_MOCK = "1234KeYiDMGoOgLeReSpOnSeMoCk";
+
+export const GOOGLE_RESPONSE_THUMBNAILLINK_MOCK = "mock://thumbnail.link";
+
 export const setupMockRequests = () => {
     nock.disableNetConnect();
     nock.enableNetConnect("127.0.0.1");
@@ -22,14 +26,14 @@ export const tokenGoogleApiRequestMock = () => {
 export const uploadGoogleApiRequestMock = () => {
     nock(`${GOOGLE_API_URL}/upload/drive/v3`)
         .post("/files?fields=id&uploadType=multipart",)
-        .reply(200, { id: "1234" });
+        .reply(200, { id: GOOGLE_RESPONSE_ID_MOCK });
 };
 
 export const getFileGoogleApiRequestMock = () => {
     nock(`${GOOGLE_API_URL}/drive/v3/files`)
-        .get("/1234?fields=id%2Cname%2CappProperties%2CthumbnailLink%2CimageMediaMetadata")
+        .get(`/${GOOGLE_RESPONSE_ID_MOCK}?fields=id%2Cname%2CappProperties%2CthumbnailLink%2CimageMediaMetadata`)
         .reply(200, {
             id: "a4123",
-            thumbnailLink: "mock://thumbnail.link=123",
+            thumbnailLink: GOOGLE_RESPONSE_THUMBNAILLINK_MOCK + "=123",
         });
 };
